a790584c3a2a44520399d39b57003e8d216434b9
aniketbumbc <aniket.umbc@gmail.com>
2025-08-15 17:32:11 +0530

    added methods

--- 01_basic/practice_numbers.py
+++ 01_basic/practice_numbers.py
@@ -1,4 +1,5 @@
 import math
+import random
 num1 = 2
 num2 = 5
 num3 = 10
@@ -12,3 +13,16 @@ print(num1,num3,num2)
 print(math.floor(4.9))
 print(math.floor(4.1))
 print(math.trunc(4.9)) # towards 0 
+
+print(oct(num1))
+print(hex(num1))
+print(bin(num1))
+
+print(random.random())
+print(random.randint(1,10))
+
+studentsRollNumbers = [40,41,42,43,44,45,50,51]
+print(random.choice(studentsRollNumbers))
+studentNames = ['a','b','c','d']
+random.shuffle(studentNames)
+print(studentNames)
