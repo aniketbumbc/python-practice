6d3ebbef79c7a9dc57393d35d039726282718769
aniketbumbc <aniket.umbc@gmail.com>
2025-08-15 17:21:32 +0530

    basic number overview

---
+++ 01_basic/practice_numbers.py
@@ -0,0 +1,14 @@
+import math
+num1 = 2
+num2 = 5
+num3 = 10
+
+print(num1 + num2 + num3)
+print((num2 + num1) * num3)
+print(num1,num3,num2)
+
+# repr, string and print difference
+
+print(math.floor(4.9))
+print(math.floor(4.1))
+print(math.trunc(4.9)) # towards 0 
