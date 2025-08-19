8836046a68625b38c988a72e5b267f91c911b987
aniketbumbc <aniket.umbc@gmail.com>
2025-08-19 12:45:02 +0530

    string code practice

--- 01_basic/practice_loop_problem.py
+++ 01_basic/practice_loop_problem.py
@@ -12,7 +12,7 @@ def count_positive(list):
 
 # count_positive(numbers_list)
 
-user_number = int(input("Please enter number"))
+# user_number = int(input("Please enter number"))
 
 def calculate_sum_even(num):
     sum = 0
@@ -22,4 +22,45 @@ def calculate_sum_even(num):
 
     print("The sum of even number is ", sum)
 
-calculate_sum_even(user_number)
+#calculate_sum_even(user_number)
+
+
+number_three = 3
+
+def number_table(num):
+    for i in range(1,11):
+        if(i == 5):
+            continue
+        print(num, 'X', i, '=', num * i)
+
+
+# number_table(number_three)
+
+# reverse string loop
+
+def reverse_str(str):
+    revr_str = ''
+    for char in str:
+        revr_str = char + revr_str
+
+    print(revr_str)
+
+
+# reverse_str('aniket')
+# reverse_str('bunny')
+# reverse_str('gfedcba')
+
+
+# find first non-repeating char
+
+def first_non_repeat_char(str):
+    for char in str:
+        if (str.count(char) == 1):
+            print('The char is ', char)
+            break
+
+
+# first_non_repeat_char("teeter")
+
+# Factorial calculator
+
