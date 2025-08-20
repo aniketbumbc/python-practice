5b65ca16de9a986665befda64eeb5d9fb1ef803f
aniketbumbc <aniket.umbc@gmail.com>
2025-08-20 09:24:10 +0530

    lambda function

---
+++ 03_functions/function_playground.py
@@ -0,0 +1,28 @@
+
+def square_of_num(num):
+    return num ** 2
+
+ans = square_of_num(4)
+
+
+
+# polymorphism in python
+
+def multiply(value1, value2):
+    return value1 * value2
+
+result = multiply(10,2)
+resultA = multiply(10,'a')
+
+print(result)
+print(resultA)
+
+# lambda functions
+
+cube = lambda x: x ** 3
+test = lambda : print("Hello Python welcome to lambda")
+addition = lambda x,y: x +y
+
+print(cube(3))
+test()
+print(addition(100,300))
