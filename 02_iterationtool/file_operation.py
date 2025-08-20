164bece3bec95801120640c8bcf0862668bdcbdd
aniketbumbc <aniket.umbc@gmail.com>
2025-08-20 08:52:45 +0530

    iteration with iter

--- 02_iterationtool/file_operation.py
+++ 02_iterationtool/file_operation.py
@@ -17,4 +17,24 @@ print(I.__next__())
 print(I.__next__())
 print(I.__next__())
 print(I.__next__())
-print(I.__next__()) # throw StopIteration exception
+#print(I.__next__()) # throw StopIteration exception
+
+D = {'a':1, 'b':2}
+
+for key in D.keys():
+    print(key)
+
+I = iter(D);
+print(next(I))
+print(next(I))
+# print(next(I))  throw StopIteration exception
+
+R = range(5)
+IR =iter(R)
+
+print(next(IR))
+print(next(IR))
+print(next(IR))
+print(next(IR))
+print(next(IR))
+# print(next(IR)) throw StopIteration exception
