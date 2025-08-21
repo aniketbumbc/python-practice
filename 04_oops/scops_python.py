7f8d9c5ecc4e69b21ec1a61270b68ec5d4974ba8
aniketbumbc <aniket.umbc@gmail.com>
2025-08-21 12:36:44 +0530

    scopes and code

---
+++ 04_oops/scops_python.py
@@ -0,0 +1,53 @@
+username = 'mike' # global scope 
+
+def scope_testing():
+    username = 'Bob inside function' # function scope 
+    city = "Baltimore"
+    print(username)
+    print (city)
+
+
+# print(city) this is not accessible here
+scope_testing()
+# print(username)
+
+x = 88
+
+def calc(y):
+    z = x + y
+    return z
+
+result = calc(100)
+# print(result)
+
+m = 200
+
+def calc2():
+    global m # global variable access here using keyword
+    m = 300
+
+# calc2()
+# print(m)
+
+# closure concept 
+
+def func1():
+    x = 9900
+    def func2():
+        print(x)
+    return func2 # pass reference fucn2
+
+result_func1 = func1()
+result_func1()
+
+def powercal(num):
+    def actual(x):
+        return x ** num
+    return actual
+
+
+get_result = powercal(3);
+get_result2 = powercal(4);
+
+print(get_result(2))
+print(get_result2(3))
