cd470cfe3448950a801fea1a6a60cab30356fa8b
aniketbumbc <aniket.umbc@gmail.com>
2025-08-22 13:44:56 +0530

    decorator

--- 04_oops/oops_python.py
+++ 04_oops/oops_python.py
@@ -1,9 +1,12 @@
 # simple class 
 
 class Car:
+    total_car = 0
+
     def __init__(self,brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
 
     def hello(self):
         print("welcome to car class")
@@ -16,6 +19,10 @@ class Car:
     
     def flue_type(self):
         return "Petrol or Diesel"
+    
+    @staticmethod
+    def general_description():
+        return "Cars mode for private and public transport"
     
 
 
@@ -30,25 +37,27 @@ class ElectricCar(Car):
 
 
 
-my_tesla = ElectricCar("Tesla", "Model s", "85Kwh")
+# my_tesla = ElectricCar("Tesla", "Model s", "85Kwh")
 
 # print(my_tesla.model)
 # print(my_tesla.full_car())
 
-print(my_tesla.flue_type())
+# print(my_tesla.flue_type())
         
 # print(my_tesla.__brand)
 # print(my_tesla.get_brand())
 
 my_car = Car("Maruti", "Swift")
+my_car_tata = Car("Tata", "Punch")
 
 print(my_car.flue_type())
-
+print(Car.total_car)
+print(Car.general_description())
 # print(my_car.brand)
 # print(my_car.model)
 # print(my_car.full_car())
 
-# my_car_tata = Car("Tata", "Punch")
+# 
 
 # print(my_car_tata.brand)
 # print(my_car_tata.model)
@@ -56,5 +65,6 @@ print(my_car.flue_type())
 
 
 # encapsulation make variable private like brand. hide details inside class 
-
+# Class variable create for total_car
 # polymorphism one method different form.
+# static method / decorator rule implementation and enhance functionality 
