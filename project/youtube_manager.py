1b7a8dd6caec1f8546bf0ad77a45c89de93c25de
aniketbumbc <aniket.umbc@gmail.com>
2025-08-24 13:18:59 +0530

    curd operation using method in python

---
+++ project/youtube_manager.py
@@ -0,0 +1,65 @@
+import json
+
+def load_data():
+    try:
+        with open('youtube.txt', 'r') as file:
+            data = json.load(file)
+            print(data)
+            return data
+    except FileNotFoundError:
+        return []
+        
+
+
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(videos,file)
+
+
+
+def list_all_videos(videos):
+    for index, video in enumerate(videos,start=1):
+        print(f" index {index}")
+
+def add_video(videos):
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name , 'time': time})
+    save_data_helper(videos)
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+
+def main():
+    videos = load_data()
+
+    while True:
+        print("Welcome to Youtube Manager | choice one option")
+        print("1. List the favorite videos")
+        print("2. Add a youtube video")
+        print("3. update video details")
+        print("4. delete a youtube video")
+        choice = input("Enter your choice")
+        print(videos)
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid choice")
+
+
+if __name__ == "__main__":
+    main()
