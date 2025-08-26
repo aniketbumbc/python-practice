fed6985a27dbb0e176f7298af38e0da1dd6a84b6
aniketbumbc <aniket.umbc@gmail.com>
2025-08-26 13:00:35 +0530

    Api handling in python

---
+++ api_handling/freeaip_users.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def fetch_random_user():
+    url = 'https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10'
+    response = requests.get(url)
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]["data"]
+        for user in user_data:
+            print(user["name"]["title"]+ ". " + user["name"]["first"] + " " + user["name"]["last"])
+        
+    else:
+        raise Exception("Failed to fetch user data")
+        
+        
+
+
+#users = data.get("data", {}).get("data", [])
+# for user in users:
+#     name = user.get("name", {})
+#     title = name.get("title", "")
+#     first = name.get("first", "")
+#     last = name.get("last", "")
+#     print(f"{title}. {first} {last}")
+
+
+
+
+fetch_random_user()
