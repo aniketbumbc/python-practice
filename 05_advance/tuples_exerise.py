6e7b76c57348b063cf71106daa2676fefd13e384
aniketbumbc <aniket.umbc@gmail.com>
2025-09-04 13:28:04 +0530

    tuple demo

---
+++ 05_advance/tuples_exerise.py
@@ -0,0 +1,31 @@
+# tuple data type immutable sequence of values.
+# can not add or delete once created. 
+
+demo_tuple = (1,2,3,4,5,4,2,1,10,4,4)
+print(demo_tuple)
+print(type(demo_tuple))
+
+print(demo_tuple[1:3])
+print(demo_tuple.index(4))
+
+print(demo_tuple.count(4))
+
+# check list palindrome or not 
+
+list_one = [1,2,4,2,1]
+
+def isListPalindrome(dummyList):
+    copy_list = dummyList[::]
+    copy_list.reverse()
+    print(copy_list)
+    if(copy_list == dummyList):
+        print("YES it is palindrome")
+    else:
+         print("NOT it is palindrome")
+
+
+
+
+
+isListPalindrome(list_one)
+isListPalindrome(["a","v","f"])
