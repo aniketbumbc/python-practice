15b70133703619d5b44977dd8e6fb5d992a5305c
aniketbumbc <aniket.umbc@gmail.com>
2026-01-18 17:32:49 +0530

    Loops added

---
+++ Practice Course/Basic/conditional.py
@@ -0,0 +1,29 @@
+# Loops practice 
+
+
+# for num in range(10):
+#     print(num)
+
+students = ["a","b","c","d"]
+
+# for v in range(len(students)):
+#     print(v)
+#     print(students[v])
+
+# for i,v in enumerate(students):
+#     print(i)
+#     print(v)
+
+
+i = 0;
+
+while i <= 5:
+    print("Hello ", i)
+    i += 1
+
+for i in range(len(students)):
+    if i == 20:
+     break
+
+else:
+   print("Hello Printing Here")
