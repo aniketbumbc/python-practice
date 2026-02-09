459dfaf6cb1c73e63bc7f5b83143e678a391264c
aniketbumbc <aniket.umbc@gmail.com>
2026-02-09 19:37:00 +0530

    http raise exception and status code

--- FastAPI/bookscrud/labbooks.py
+++ FastAPI/bookscrud/labbooks.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field, ConfigDict
+from starlette import status
 
 app = FastAPI()
 
@@ -16,10 +17,6 @@ class LABBOOK:
         self.author = author
         self.isPublished = isPublished
     
-
-
-
-
 
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description='ID is not present to created', default=None)
@@ -38,12 +35,7 @@ class BookRequest(BaseModel):
 }
 
 
-
-
-
-
 LAB_BOOKS = [
-
     LABBOOK(1,'Computer Sciences', 'codewithpi', True),
     LABBOOK(2,'Fast API Sciences', 'codewithpi', False),
     LABBOOK(3,'Computer Sciences-1', 'meApi', True),
@@ -52,15 +44,48 @@ LAB_BOOKS = [
 ]
 
 
-@app.get('/labbook')
+@app.get('/labbook',status_code=status.HTTP_200_OK)
 async def read_all_books():
     return LAB_BOOKS
 
-@app.post('/create_book')
+
+
+
+
+@app.post('/create_book',status_code=status.HTTP_201_CREATED)
 async def create_book(book_request:BookRequest):
     new_book = LABBOOK(**book_request.dict())
     LAB_BOOKS.append(get_book_id(new_book))
     return LAB_BOOKS
+    
+
+
+@app.get('/labbook/{labbook_id}',status_code=status.HTTP_200_OK)
+async def get_book_by_id(labbook_id:int = Path(gt=0,default=None)):
+    for book in LAB_BOOKS:
+        if book.id == labbook_id:
+            return book
+
+    raise HTTPException(status_code=404, detail='book not found')
+
+@app.get('/labbook/',status_code=status.HTTP_200_OK)
+async def get_book_by_name(
+    
+    name:str = Query(default=None,min_length=3),
+   
+):
+    for book in LAB_BOOKS:
+        if book.title == name:
+            return book
+
+@app.delete('/labbook/{labbook_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book_by_id(labbook_id:int = Path(gt=0,default=None)):
+    for book in LAB_BOOKS:
+        if book.id == labbook_id:
+            LAB_BOOKS.remove(book)
+            return LAB_BOOKS
+    raise HTTPException(status_code=404, detail='book not found')
+
 
 
 def get_book_id(labbook:LABBOOK):
