0b8437e80b5071407315f1e905b5f70974fbbdd1
aniketbumbc <aniket.umbc@gmail.com>
2026-02-12 13:14:18 +0530

    request model and post,delete,put api handle

--- FastAPI/TodoApp/main.py
+++ FastAPI/TodoApp/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 from database import engine, SessionLocal
 import models
 from sqlalchemy.orm import Session
 from typing import Annotated
+from pydantic import BaseModel, Field
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,12 +17,59 @@ def get_db():
         db.close()
 
 
-db_dependency = Annotated[Session, Depends(get_db)]
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool = Field(default=False)
 
 
-@app.get("/")
-async def read_all_todos(db: db_dependency):
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all_todos(db: Session = Depends(get_db)):
     todo_list = db.query(models.Todo).all()
     return todo_list
 
 
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(todo_id: Annotated[int, Path(gt=0,default=None)], db: Session = Depends(get_db)):
+    todo_item = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo_item is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return todo_item
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_request: TodoRequest, db: Session = Depends(get_db)):
+    new_todo = models.Todo(**todo_request.dict())
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+    if new_todo is not None:
+        return new_todo
+    else:
+        raise HTTPException(status_code=400, detail="Todo not created")
+
+
+@app.put("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def update_todo(todo_id: Annotated[int, Path(gt=0,default=None)], todo_request: TodoRequest, db: Session = Depends(get_db)):
+    todo_item = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo_item is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    todo_item.title = todo_request.title
+    todo_item.description = todo_request.description
+    todo_item.priority = todo_request.priority
+    todo_item.complete = todo_request.complete
+
+    db.commit()
+    return todo_item
+ 
+
+@app.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: Annotated[int, Path(gt=0,default=None)], db: Session = Depends(get_db)):
+    todo_item = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if todo_item is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.query(models.Todo).filter(models.Todo.id == todo_id).delete()
+    db.commit()
+
+    return {"message": "Todo deleted successfully"}
