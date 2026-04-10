1ff0c37008a0a78492f93749ef9edcde05eecb1d
aniketbumbc <aniket.umbc@gmail.com>
2026-04-10 15:30:05 +0530

    added db connection with query for get all products.

--- FastAPI/product-curd/database.py
+++ FastAPI/product-curd/database.py
@@ -4,4 +4,4 @@ from sqlalchemy import create_engine
 db_url = "postgresql://postgres:123@localhost:5432/fastapi"
 engine = create_engine(db_url)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- FastAPI/product-curd/main.py
+++ FastAPI/product-curd/main.py
@@ -1,22 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models import Product
-from database import SessionLocal
+from database import session,engine
+import database_models
+from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
-
-product_list = [
-Product(id=10, name="Laptop", description="2026 Laptop", price=33.00, quantity=10),
-Product(id=11, name="Keyboard", description="Mechanical Keyboard", price=15.00, quantity=25),
-Product(id=12, name="Mouse", description="Wireless Mouse", price=10.00, quantity=30),
-Product(id=13, name="Monitor", description="24-inch LED Monitor", price=120.00, quantity=15),
-Product(id=14, name="Printer", description="All-in-One Inkjet Printer", price=80.00, quantity=8),
-Product(id=15, name="Scanner", description="Flatbed Document Scanner", price=60.00, quantity=10),
-]
+database_models.Base.metadata.create_all(bind=engine) # create table into db
 
 
+# product_list = [
+# Product(id=10, name="Laptop", description="2026 Laptop", price=33.00, quantity=10),
+# Product(id=11, name="Keyboard", description="Mechanical Keyboard", price=15.00, quantity=25),
+# Product(id=12, name="Mouse", description="Wireless Mouse", price=10.00, quantity=30),
+# Product(id=13, name="Monitor", description="24-inch LED Monitor", price=120.00, quantity=15),
+# Product(id=14, name="Printer", description="All-in-One Inkjet Printer", price=80.00, quantity=8),
+# Product(id=15, name="Scanner", description="Flatbed Document Scanner", price=60.00, quantity=10),
+# ]
 
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    db = session()
+    count= db.query(database_models.Product).count()    
+    if count == 0:
+        for product in product_list:
+            db.add(database_models.Product(**product.model_dump()))
+
+    db.commit()
+
+
+init_db()
 
 @app.get("/")
 def greet ():
@@ -24,14 +45,17 @@ def greet ():
 
 
 @app.get("/products")
-def get_all_products():
-    return product_list
+def get_all_products(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 @app.get("/product/{id}")
-def get_product(id:int):
-    for product in product_list:
-        if product.id == id:
-            return product
+def get_product(id:int,db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        return db_product
+
+
     return "No Product Found"
 
 @app.post("/product")
